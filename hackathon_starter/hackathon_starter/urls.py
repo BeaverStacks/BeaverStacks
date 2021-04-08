152f9bed304d3b5d9ab7b5a1ab4070d4b2d65936
Asa Holland <hollandasa@gmail.com>
2021-04-08 04:31:46 -0700

    Update urls.py
    Needed to update the format of how the urlpatterns are saved. This looks like it has changed in the last two years since the repo was active.

--- hackathon_starter/hackathon_starter/urls.py
+++ hackathon_starter/hackathon_starter/urls.py
@@ -1,10 +1,11 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 from hackathon import views
 
-urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+urlpatterns = [url(r'^$', views.index, name='index'),
     url(r'^hackathon/', include('hackathon.urls')),
-    url(r'^admin/', include(admin.site.urls)),
-    # url(r'^openid/(.*)', SessionConsumer()),
-)
+
+    # Changed based on https://stackoverflow.com/questions/43324005/django-deprecation-warning-or-improperlyconfigured-error-passing-a-3-tuple-to
+    # It appears this format has changed since the repo was made
+    url(r'^admin/', admin.site.urls)]
+    
